89ee02fd36fcf3ae5a92d49203bc769c33207cfc
rakesh8874 <rakeshtiwari887429@gmail.com>
2024-01-26 23:39:21 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Skills from './components/Skills/Skills';
+import Qulification from './components/Qualification/Qualification';
+import Contact from './components/Contant/Contact'
+import Footer from './components/Footer/Footer';
+import ScrollUp from './components/ScrollUp/ScrollUp';
+import Work from './components/Work/Work';
+
+function App() {
+  return (
+     <>
+     <Header />
+     <main className='main'>
+      <Home />
+      <About />
+      <Skills />
+      <Qulification />
+      <Work />
+      <Contact />
+     </main>
+      <Footer />
+      <ScrollUp />
+     </>
+  );
+}
+
+export default App;
